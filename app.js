ed552a20b7e1abbc8177f46b057be7548e44c34b
Lwelch45 <lwelch1@ithaca.edu>
2015-03-16 09:29:17 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+//set vars for environment
+var express = require('express');
+var app = express();
+var path = require('path');
+
+//set server port
+app.listen(4000);
+console.log('server is listening');
